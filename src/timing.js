fcfc02fb5bf1e185281a6199e863b25d2d48e2e6
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-07-14 20:41:26 -0300

    finishing

---
+++ src/timing.js
@@ -0,0 +1,23 @@
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("second").innerHTML = "EXPIRED";
+  }
+}, 1000);
